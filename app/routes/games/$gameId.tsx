a60eaa14b0ac3a50e22c93fc1d29894c0474e0e2
Christopher Moore <cwm322@gmail.com>
2022-09-09 17:20:28 +0100

    re-open game from the game screen

--- app/routes/games/$gameId.tsx
+++ app/routes/games/$gameId.tsx
@@ -1,9 +1,11 @@
 import type { LoaderArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
-import { getGame } from "~/models/game.server";
+import { getGame, reopenGame } from "~/models/game.server";
+import { getNextPlayerToPlay } from "~/game-utils";
 
 export const loader = async ({ params }: LoaderArgs) => {
   invariant(params.gameId, "gameId not found");
@@ -18,6 +20,25 @@ export const loader = async ({ params }: LoaderArgs) => {
   return json({ game });
 };
 
+export const action = async ({ request, params }: ActionArgs) => {
+  invariant(params.gameId, "gameId not found");
+  const { gameId } = params;
+
+  const game = await getGame({ id: params.gameId });
+  if (!game) {
+    throw new Response("Not Found", { status: 404 });
+  }
+
+  const formData = await request.formData();
+
+  if (formData.get("action") === "reopen") {
+    await reopenGame(gameId);
+    const nextPlayer = getNextPlayerToPlay(game);
+
+    return redirect(`/games/${gameId}/play/${nextPlayer.id}`);
+  }
+};
+
 export default function GamePage() {
   const { game } = useLoaderData<typeof loader>();
 
@@ -29,13 +50,25 @@ export default function GamePage() {
           {game.players[0].totalScore}
         </h2>
       ) : (
-        <h2 className="text-3xl">"Still Running"</h2>
+        <h2 className="text-3xl">Still Playing</h2>
       )}
       {game.players.map((p) => (
         <p key={p.id}>
           {p.name} {p.totalScore}
         </p>
       ))}
+      {game.completed && (
+        <Form method="post" action="">
+          <button
+            type="submit"
+            className="rounded bg-green-500 py-2 px-4 text-white hover:bg-green-600 focus:bg-green-400"
+            name="action"
+            value="reopen"
+          >
+            Re-open game
+          </button>
+        </Form>
+      )}
     </>
   );
 }
