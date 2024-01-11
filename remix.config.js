b2835f3b3a9cf21f31a972fa40d836f6ee749157
Christopher Moore <cwm322@gmail.com>
2024-01-11 17:01:48 +0000

    v2 changes

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,25 @@
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  ignoredRouteFiles: ["**/*"],
+  serverModuleFormat: 'cjs',
   future: {
     unstable_tailwind: true,
     v2_errorBoundary: true,
     v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+    v2_headers: true,
+    v2_dev: true,
+  },
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes, {
+      ignoredFilePatterns: [".*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+    });
   },
 };
