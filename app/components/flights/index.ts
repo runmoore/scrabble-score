c06856085276f732c69800ef1239b505bb510593
Christopher Moore <cwm322@gmail.com>
2026-02-17 15:51:22 +0000

    Revert "feat: add flights booking routes with modal and standalone views (#7)" (#8)
    This reverts commit cb844aa335690ec2236940c46560960b1973ffee.

--- app/components/flights/index.ts
+++
@@ -1,6 +0,0 @@
-export { FlightCard } from "./FlightCard";
-export { FlightDetails } from "./FlightDetails";
-export { FlightTimeline } from "./FlightTimeline";
-export { Modal, useModal } from "./Modal";
-export { PartnerList } from "./PartnerList";
-export { SearchHeader } from "./SearchHeader";
